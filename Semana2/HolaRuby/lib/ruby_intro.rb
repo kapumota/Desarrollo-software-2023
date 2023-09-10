43ca5442998a7e08c7761668855000a8a0599aaa
kapumota <claraa@uni.edu.pe>
2023-09-10 11:37:49 -0500

    Archivo inicial de la actividad

---
+++ Semana2/HolaRuby/lib/ruby_intro.rb
@@ -0,0 +1,33 @@
+# Parte 1
+
+def sum arr
+  # COMPLETA TU CODIGO
+end
+
+def max_2_sum arr
+  # COMPLETA TU CODIGO
+end
+
+def sum_to_n? arr, n
+  # COMPLETA TU CODIGO
+end
+
+# Parte 2
+
+def hello(name)
+  # COMPLETA TU CODIGO
+end
+
+def starts_with_consonant? s
+  # COMPLETA TU CODIGO
+end
+
+def binary_multiple_of_4? s
+  # COMPLETA TU CODIGO
+end
+
+# Parte 3
+
+class BookInStock
+# COMPLETA TU CODIGO
+end
