72205f94a3769422edd6101cc1e49de96fd8aecd
kapumota <121532729+kapumota@users.noreply.github.com>
2023-09-11 16:18:51 -0500

    Parte 1

--- Semana2/HolaRuby/spec/parte1_spec.rb
+++ Semana2/HolaRuby/spec/parte1_spec.rb
@@ -8,14 +8,14 @@ describe 'Introduccion a Ruby' do
       expect { sum([1, 3, 4]) }.not_to raise_error
     end
 
-    it 'retorna la suma correcta', points: 20 do
+    it 'retorna la suma correcta'  do
       expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
       expect(sum([1, 2, 3, 4, 5])).to eq(15)
       expect(sum([1, 2, 3, 4, -5])).to eq(5)
       expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
     end
 
-    it 'trabaja sobre el arreglo vacio', points: 10 do
+    it 'trabaja sobre el arreglo vacio' do
       expect { sum([]) }.not_to raise_error
       expect(sum([])).to be_zero
     end
@@ -25,17 +25,17 @@ describe 'Introduccion a Ruby' do
     it 'deberia ser definido' do
       expect { max_2_sum([1, 2, 3]) }.not_to raise_error
     end
-    it 'retorna la suma correcta', points: 7 do
+    it 'retorna la suma correcta'  do
       expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
       expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
     end
-    it 'trabaja incluso si los dos valores mas grandes son lo mismo', points: 3 do
+    it 'trabaja incluso si los dos valores mas grandes son lo mismo' do
       expect(max_2_sum([1, 2, 3, 3])).to eq(6)
     end
-    it 'retorna cero si el arreglo es vacio', points: 10 do
+    it 'retorna cero si el arreglo es vacio' do
       expect(max_2_sum([])).to be_zero
     end
-    it 'retorna el valor del elemento si es solo un elemento', points: 10 do
+    it 'retorna el valor del elemento si es solo un elemento' do
       expect(max_2_sum([3])).to eq(3)
     end
   end
@@ -44,20 +44,20 @@ describe 'Introduccion a Ruby' do
     it 'deberia ser definido' do
       expect { sum_to_n?([1, 2, 3], 4) }.not_to raise_error
     end
-    it 'retorna verdadero cuando dos elementos cualesquiera se suman al segundo argumento', points: 30 do
+    it 'retorna verdadero cuando dos elementos cualesquiera se suman al segundo argumento' do
       expect(sum_to_n?([1, 2, 3, 4, 5], 5)).to be true # 2 + 3 = 5
       expect(sum_to_n?([3, 0, 5], 5)).to be true # 0 + 5 = 5
-      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], -3)).to be true  # handles negative sum
-      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], 12)).to be false # 3 + 4 + 5 = 12 (not 3 elements)
-      expect(sum_to_n?([-1, -2, 3, 4, 6, -8], 12)).to be false # no two elements that sum
+      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], -3)).to be true  # maneja la suma negativa
+      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], 12)).to be false # 3 + 4 + 5 = 12 
+      expect(sum_to_n?([-1, -2, 3, 4, 6, -8], 12)).to be false # no dos elementos que sumar
     end
-    it 'retorna falso para un unico elemento del arreglo', points: 5 do
+    it 'retorna falso para un unico elemento del arreglo' do
       expect(sum_to_n?([0], 0)).to be false
       expect(sum_to_n?([1], 1)).to be false
       expect(sum_to_n?([-1], -1)).to be false
       expect(sum_to_n?([-3], 0)).to be false
     end
-    it 'retorna falso para un arreglo vacior', points: 5 do
+    it 'retorna falso para un arreglo vacior' do
       expect(sum_to_n?([], 0)).to be false
       expect(sum_to_n?([], 7)).to be false
     end
