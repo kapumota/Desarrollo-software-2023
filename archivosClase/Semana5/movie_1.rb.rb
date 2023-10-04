23344921bd6105c32c809117d630b0a9ba92c655
kapumota <121532729+kapumota@users.noreply.github.com>
2023-10-04 11:40:19 -0500

    Archivo 4

--- archivosClase/Semana5/movie_1.rb.rb
+++ archivosClase/Semana5/movie_1.rb.rb
@@ -1,3 +1,11 @@
+# Correccion André Calagua
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date
+    private
+    def movie_params
+    params.require(:movie).permit( :title, :rating, :description, :release_date)
+  end
 end
+
+#class Movie < ActiveRecord::Base
+# attr_accessible :title, :rating, :description, :release_date
+#end
