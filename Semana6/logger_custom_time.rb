957657ea628ac8339efbac612eb9fa245853ea29
kapumota <121532729+kapumota@users.noreply.github.com>
2023-10-30 10:23:11 -0500

    Codigo 6

--- Semana6/logger_custom_time.rb
+++ Semana6/logger_custom_time.rb
@@ -3,3 +3,10 @@ require 'logger'
 logger = Logger.new(STDOUT)
 logger.datetime_format = "%I:%M:%S%P "
 logger.debug("Usuario 23643 logged")
+
+=begin
+require 'logger' 
+logger = Logger.new(STDOUT) 
+logger.datetime_format = "%h %d %Y - %I: %M: %S%P " 
+logger.debug("Usuario 23643 logged") 
+=end
