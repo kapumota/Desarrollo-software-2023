715c9a9487a5eb17a9f354f5b0f60bae0b04b42f
kapumota <121532729+kapumota@users.noreply.github.com>
2023-10-30 10:35:42 -0500

    Codigo 7

--- Semana6/logger_custom_timezone.rb
+++ Semana6/logger_custom_timezone.rb
@@ -4,4 +4,4 @@ logger = Logger.new(STDOUT)
 logger.formatter = proc do |severity, datetime, progname, msg|
   "#{severity} | #{datetime.zone}: #{datetime} | UTC: #{datetime.utc} | #{msg}\n"
 end
-logger.debug("User 23643 logged in")
+logger.debug("Usuario 23643 logged")
